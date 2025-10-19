4b6c6cd7c4373d1de6b3e6fd26cae0615108876a
Petar Obradovic <perazmn@gmail.com>
2025-10-19 19:23:42 +0200

    Add ScannerCore

--- src/root.zig
+++ src/root.zig
@@ -1,7 +1,9 @@
 //! By convention, root.zig is the root source file when making a library.
 const std = @import("std");
 pub const preprocessor = @import("preprocessor.zig");
+pub const lexer = @import("lexer.zig");
 
 test "import" {
     _ = @import("preprocessor.zig");
+    _ = @import("lexer.zig");
 }
